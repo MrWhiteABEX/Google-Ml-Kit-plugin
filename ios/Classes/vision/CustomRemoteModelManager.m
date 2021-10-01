64e0f7c3c74896d1f7ef8841fed9683aea82d4df
MrWhiteABEX <71495952+MrWhiteABEX@users.noreply.github.com>
2021-10-01 17:14:49 +0200

    Update CustomRemoteModelManager.m

--- ios/Classes/vision/CustomRemoteModelManager.m
+++ ios/Classes/vision/CustomRemoteModelManager.m
@@ -1,7 +1,6 @@
 #import "GoogleMlKitPlugin.h"
 #import "GenericModelManager.h"
 #import <MLKitCommon/MLKitCommon.h>
-#import <MLKitLinkFirebase/MLKitLinkFirebase.h>
 
 #define manageRemoteModel @"vision#manageRemoteModel"
 
@@ -14,19 +13,10 @@
 }
 
 - (void)handleMethodCall:(FlutterMethodCall *)call result:(FlutterResult)result {
-    if ([call.method isEqualToString:manageRemoteModel]) {
-        [self manageModel:call result:result];
-    } else {
-        result(FlutterMethodNotImplemented);
-    }
+    result(FlutterMethodNotImplemented);
 }
 
 - (void)manageModel:(FlutterMethodCall *)call result:(FlutterResult)result {
-    NSString *modelTag = call.arguments[@"model"];
-    MLKFirebaseModelSource *firebaseModelSource = [[MLKFirebaseModelSource alloc] initWithName:modelTag];
-    MLKCustomRemoteModel *model = [[MLKCustomRemoteModel alloc] initWithRemoteModelSource:firebaseModelSource];
-    genericModelManager = [[GenericModelManager alloc] init];
-    [genericModelManager manageModel:model call:call result:result];
 }
 
 @end
